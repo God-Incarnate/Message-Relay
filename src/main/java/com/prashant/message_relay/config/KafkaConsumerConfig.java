04824c139da45f3aebcbfb5582b26076d51667ac
God-Incarnate <prashant11111999@gmail.com>
2026-03-23 21:08:31 +0530

    Kafka consumer config

--- src/main/java/com/prashant/message_relay/config/KafkaConsumerConfig.java
+++ src/main/java/com/prashant/message_relay/config/KafkaConsumerConfig.java
@@ -25,5 +25,22 @@ public class KafkaConsumerConfig {
     @Value("${kafka.topics.dlq}")
     private String dlqTopic;
 
+    @Bean
+    public ConsumerFactory<String, NotificationEvent> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "delivery-engine-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
 
+        JsonDeserializer<NotificationEvent> deserializer = new JsonDeserializer<>(NotificationEvent.class);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeHeaders(false);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    
 }
