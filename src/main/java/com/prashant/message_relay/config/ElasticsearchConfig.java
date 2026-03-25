1a9f49c2993a5a23dbe27e1521a9bd91aeebd655
God-Incarnate <prashant11111999@gmail.com>
2026-03-25 12:18:32 +0530

    Elasticsearch Config

---
+++ src/main/java/com/prashant/message_relay/config/ElasticsearchConfig.java
@@ -0,0 +1,12 @@
+package com.prashant.message_relay.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+
+}
