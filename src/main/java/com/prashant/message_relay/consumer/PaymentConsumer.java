3530425df5ef0f035249cede778848388d07284a
God-Incarnate <prashant11111999@gmail.com>
2026-03-25 12:16:12 +0530

    kafka Payment consumer

--- src/main/java/com/prashant/message_relay/consumer/PaymentConsumer.java
+++ src/main/java/com/prashant/message_relay/consumer/PaymentConsumer.java
@@ -22,5 +22,17 @@ public class PaymentConsumer {
         groupId = "delivery-engine-group",
         concurrency = "3"
     )
-
+    public void consume(NotificationEvent event,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+                        @Header(KafkaHeaders.OFFSET) long offset,
+                        Acknowledgment ack) {
+        log.debug("Payment consumer received eventId={} partition={} offset={}",
+                event.getEventId(), partition, offset);
+        try {
+            deliveryService.process(event);
+            ack.acknowledge();
+        } catch (Exception e) {
+            log.error("Payment consumer error eventId={} error={}", event.getEventId(), e.getMessage());
+        }
+    }
 }
